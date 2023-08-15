2583818b4045911e5e4315cf76ce4a6a3f20b459
Shreya Sagar <shreyasagar1705@gmail.com>
2023-08-15 17:35:01 +0530

    Updated Linked List

--- src/main/java/com/learning/datastructure/LinkedList.java
+++ src/main/java/com/learning/datastructure/LinkedList.java
@@ -38,7 +38,7 @@ public class LinkedList {
 
   public void append(int value) {
     Node newNode = new Node(value);
-    if(length == 0) {
+    if (length == 0) {
       head = newNode;
       tail = newNode;
     } else {
@@ -49,17 +49,19 @@ public class LinkedList {
   }
 
   public Node removeLast() {
-    if(length == 0) return null;
+    if (length == 0) {
+      return null;
+    }
     Node pre = head;
     Node temp = head;
-    while(temp.next != null) {
+    while (temp.next != null) {
       pre = temp;
       temp = temp.next;
     }
     tail = pre;
     tail.next = null;
     length--;
-    if(length == 0) {
+    if (length == 0) {
       head = null;
       tail = null;
     }
@@ -68,7 +70,7 @@ public class LinkedList {
 
   public void prepend(int value) {
     Node newNode = new Node(value);
-    if(length == 0) {
+    if (length == 0) {
       head = newNode;
       tail = newNode;
     } else {
@@ -79,13 +81,102 @@ public class LinkedList {
   }
 
   public Node removeFirst() {
-    if(length == 0) return null;
+    if (length == 0) {
+      return null;
+    }
     Node temp = head;
     head = head.next;
     temp.next = null;
     length--;
-    if(length == 0) tail = null;
+    if (length == 0) {
+      tail = null;
+    }
     return temp;
+  }
+
+  public Node get(int index) {
+    if (index < 0 | index >= length) {
+      return null;
+    }
+    Node temp = head;
+    for (int i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+
+  public boolean set(int index, int value) {
+    Node temp = get(index);
+    if (temp != null) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  public boolean insert(int index, int value) {
+    if (index < 0 || index > length) {
+      return false;
+    }
+    if (index == 0) {
+      prepend(value);
+      return true;
+    }
+    if (index == length) {
+      append(value);
+      return true;
+    }
+    Node newNode = new Node(value);
+    Node temp = get(index - 1);
+    newNode.next = temp.next;
+    temp.next = newNode;
+    length++;
+    return true;
+  }
+
+  public Node remove(int index) {
+    if (index < 0 || index >= length) {
+      return null;
+    }
+    if (index == 0) {
+      return removeFirst();
+    }
+    if (index == length - 1) {
+      return removeLast();
+    }
+
+    Node prev = get(index - 1);
+    Node temp = prev.next;
+    prev.next = temp.next;
+    temp.next = null;
+    length--;
+    return temp;
+  }
+
+  public void reverse() {
+    Node temp = head;
+    head = tail;
+    tail = temp;
+
+    Node before = null;
+    for (int i = 0; i < length; i++) {
+      Node after = temp.next;
+      temp.next = before;
+      before = temp;
+      temp = after;
+    }
+  }
+
+  public Node findMiddleNode() {
+    if(length == 0) return null;
+    if(length == 1) return head;
+    Node temp = head;
+    if(length % 2 == 0) {
+      for (int i = 1; i < length/2; i++) {
+
+      }
+    }
+    return get(length / 2);
   }
 
   public class Node {
--- src/main/java/com/learning/datastructure/Main.java
+++ src/main/java/com/learning/datastructure/Main.java
@@ -9,14 +9,37 @@ public class Main {
 
     LinkedList linkedList = new LinkedList(5);
     linkedList.append(8);
-    linkedList.append(12);
-    System.out.println("Removed last element " + linkedList.removeLast().value);
     linkedList.prepend(6);
+    linkedList.printList();
+
+    System.out.println("Removed last element " + linkedList.removeLast().value);
     System.out.println("Removed first element " + linkedList.removeFirst().value);
 
     System.out.println("Head : " + linkedList.getHead().value);
     System.out.println("Tail : " + linkedList.getTail().value);
     System.out.println("Length : " + linkedList.getLength());
     linkedList.printList();
+
+    System.out.println("GET : " + linkedList.get(linkedList.getLength() - 1).value);
+    System.out.println("INSERT : " + linkedList.insert(0, 101));
+    linkedList.printList();
+
+    System.out.println("SET : " + linkedList.set(linkedList.getLength() - 1, 201));
+    linkedList.printList();
+    System.out.println("APPEND");
+    linkedList.append(222);
+    linkedList.append(333);
+    linkedList.append(444);
+    linkedList.append(000);
+    linkedList.append(555);
+    linkedList.append(666);
+    linkedList.printList();
+    System.out.println("REMOVE : " + linkedList.remove(linkedList.getLength() - 1).value);
+    linkedList.printList();
+    linkedList.removeFirst();
+    System.out.println("REVERSE");
+    linkedList.reverse();
+    linkedList.printList();
+    System.out.println("MIDDLE NODE : "+linkedList.findMiddleNode().value);
   }
 }
